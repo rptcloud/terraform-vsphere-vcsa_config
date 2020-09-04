15fc3885737c10a2c12730366622eb6cf5b3612a
Frank Parry <fparry@riverpointtechnology.com>
2020-09-04 11:24:22 -0400

    add MIT license information

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+################################################################################
+################################################################################
+################################################################################
+###                                                                          ###
+### Name: terraform-vsphere-vcsa_config                                      ###
+### Description: [Terraform] Module to configure VMware VCSA, join nested    ###
+###   ESXi hosts to cluster                                                  ###
+### Last Modified: fparry(2020-09-04T11:12:56-04:00)                         ###
+### License: MIT (See LICENSE.txt in the root of this repository for more    ###
+###   information.)                                                          ###
+###                                                                          ###
+################################################################################
+################################################################################
+################################################################################
+
 provider "vsphere" {
   user           = data.terraform_remote_state.vmware_tfc_env1.outputs.vcenter_username
   password       = data.terraform_remote_state.vmware_tfc_env1.outputs.vcenter_password
